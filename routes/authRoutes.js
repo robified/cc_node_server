933cb6ec26fea50d3ba6cea82d245abe26ce2e7d
Robin <daorob@gmail.com>
2020-04-01 18:39:38 -0500

    Implement a logout route, and able to deny request if a user is not authenticated.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 
-module.exports = (app) => {
+module.exports = app => {
     app.get(
         '/auth/google',
         passport.authenticate('google', {
@@ -10,8 +10,11 @@ module.exports = (app) => {
 
     app.get('/auth/google/callback', passport.authenticate('google'));
 
-    // this can be any route you want
-    app.get('/api/current_user', (req, res) => {
+    app.get('/api/logout', (req, res) => {
+        req.logout();
         res.send(req.user);
     });
+
+    // this can be any route you want
+    app.get('/api/current_user', (req, res) => res.send(req.user));
 };
