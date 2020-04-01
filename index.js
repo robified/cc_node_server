d45186312c04f966e92fef9c3d0953f266f3bb10
Robin <daorob@gmail.com>
2020-04-01 04:24:07 -0500

    Install/require passport and passport-google-oauth20. Configure passport to use GoogleStrategy.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send( { bye: 'omni' } );
-});
+// "passport, i want you to be aware that there is a new strategy available"
+// this create a new instance of the passport strategy
+// "hey application, I want to somehow authenticate my users with Google"
+// inside the constructor, we're going to pass in some configuration that tells GoogleStrategy how to authenticate users inside of the application
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
