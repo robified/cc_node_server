a4e268b26a1dfa4872ba7ae1566cdea7a6cad7ae
Robin <daorob@gmail.com>
2020-04-01 06:07:10 -0500

    Refactor code for passport services, and authRoutes into a separate folder.

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
 const app = express();
 
-// "passport, i want you to be aware that there is a new strategy available"
-// this create a new instance of the passport strategy
-// "hey application, I want to somehow authenticate my users with Google"
-// inside the constructor, we're going to pass in some configuration that tells GoogleStrategy how to authenticate users inside of the application
-passport.use(
-    // GoogleStrategy has an internal identified of 'google'
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        },
-        (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile:', profile);
-        }
-    )
-);
-
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
