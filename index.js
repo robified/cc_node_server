a8933d703b0c5da9aec0ef05d1259c9aa5badb43
Robin <daorob@gmail.com>
2020-04-01 04:47:37 -0500

    Set up Google Strategy Options with clientID, clientSecret, and callbackURL.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
@@ -8,7 +9,18 @@ const app = express();
 // this create a new instance of the passport strategy
 // "hey application, I want to somehow authenticate my users with Google"
 // inside the constructor, we're going to pass in some configuration that tells GoogleStrategy how to authenticate users inside of the application
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
